5ca80890033c0ddbc9f1a14e37b18da14ff954d6
sh3ki <shekaigarcia@gmail.com>
2025-12-11 12:21:19 +0800

    Update urls.py

--- issc/main/urls.py
+++ issc/main/urls.py
@@ -27,6 +27,8 @@ urlpatterns = [
     path("import/",auth_view.import_data, name='import-forms'),
     path("download-template/", auth_view.download_template, name='download-template'),
     path('get-user/', auth_view.getUser, name='get_user'),
+    path('get-admin-contact/', auth_view.get_admin_contact, name='get_admin_contact'),
+    path('save-admin-contact/', auth_view.save_admin_contact, name='save_admin_contact'),
 
     path("account/password-reset/", auth_view.CustomPasswordResetView.as_view(), name="password_reset"),
     # path("account/password-reset/done/", auth_view.CustomPasswordResetDoneView.as_view(), name="password_reset_done"),
